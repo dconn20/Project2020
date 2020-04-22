6961b0360db4aa9ba1bc7dbcbcb78b436aebe3ba
dconn20 <damien.connolly85@gmail.com>
2020-04-22 13:06:34 +0100

    summary.txt

--- test.py
+++ test.py
@@ -2,19 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import sys 
+
+sys.stdout = open("summaryfile.txt", "w")
 
 iris=pd.read_csv("iris.csv")
 
 iris.columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
 
-#you can specific the number to show here
-#iris.head(150)
-
 iris.shape
 
 iris['species'].unique()
-print(iris.groupby('species').size())
 
+#sys.stdout = open("summaryfile.txt", "w")
+print(iris.groupby('species').size())
 
 
 iris.min()
@@ -29,11 +30,12 @@ print(summary)
 
 byspecies = iris.groupby('species')
 table2 = byspecies.describe()
+pd.set_option('display.max_columns', None)
 print(table2)
 
 
-print(iris.head(150))
-#with open("iris.csv") as f:
- #   with open("summary.txt", "w") as f1:
-  #      for line in f:
-    #        f1.write(summary)
+sys.stdout.close()
+
+
+ 
+
